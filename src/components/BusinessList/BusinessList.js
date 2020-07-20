ed29e552245cddd831c9a0ad2681b659977ae566
djghis <djghis@hotmail.com>
2020-07-20 14:45:40 +0100

    hard coded <Business /> moved to be rendered by array in app.js

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -6,13 +6,13 @@ class BusinessList extends React.Component {
     render () {
         return(
             <div className="BusinessList">
-  <Business /> 
-  <Business />
-  <Business />
-  <Business />
-  <Business />
-  <Business />
-</div>
+                <Business /> 
+                <Business />
+                <Business />
+                <Business />
+                <Business />
+                <Business />
+            </div>
         )
     }
 };
