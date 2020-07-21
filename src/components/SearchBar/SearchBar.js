15f6722ef1913cc8acbeba2f137bddb93640a4c0
djghis <djghis@hotmail.com>
2020-07-21 15:04:12 +0100

    search bar changing color when slected option and using the state and css to do so

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,17 +1,39 @@
 import React from 'react';
 import './SearchBar.css';
 
-const sortByOptions = {
-    'Best Match': 'best_match',
-    'Highest rated': 'rating',
-    'Most Reviewed': 'review_count'
-};
+
 
 class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            term: "",
+            location: "",
+            sortBy: "best_match"
+        }
+        this.sortByOptions= {
+            'Best Match': 'best_match',
+            'Highest rated': 'rating',
+            'Most Reviewed': 'review_count'
+        }
+    };
+    
+    getSortByClass(sortByOption) {
+        if(this.state.sortBy === sortByOption) {
+            return 'active'
+        } else {
+            return ''
+        };
+    }
+
+    handleSortBychange(sortByOption){
+        this.setState({sortBy: sortByOption})
+    }
+
     renderSortByOptions() {
-        return Object.keys(sortByOptions).map(sortByOption => {
-            let sortByOptionValue = sortByOptions[sortByOption];
-        return  <li key={sortByOptionValue}>{sortByOption}</li>
+        return Object.keys(this.sortByOptions).map(sortByOption => {
+            let sortByOptionValue = this.sortByOptions[sortByOption];
+        return  <li  className={this.getSortByClass(sortByOptionValue)} key={sortByOptionValue} onClick={this.handleSortBychange.bind(this, sortByOptionValue)}>{sortByOption}</li>
         }); 
     }
 
