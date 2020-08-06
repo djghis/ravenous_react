f55fbabedab3ea332b46f071852fe24d2f1db961
djghis <djghis@hotmail.com>
2020-08-06 13:31:39 +0100

    api linked but corrs issues

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,35 +3,30 @@ import React from 'react';
 import './App.css';
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
-
-const business = {
-  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-name: 'MarginOtto Pizzeria',
-address: '1010 Paddington Way',
-city: 'Flavortown',
-state: 'NY',
-zipCode: '10101',
-category: 'Italian',
-rating: 4.5,
-reviewCount: 90
-};
+import Yelp from '../../util/Yelp';
 
 
-
-let businesses = [business, business, business, business, business, business];
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {businesses: []};
+
+    this.searchYelp = this.searchYelp.bind(this);
+  }
 
   searchYelp(term, location, sortBy){
-   console.log(`I sm searching yelps with ${term}, ${location} and ${sortBy}.`)
+   Yelp.search(term, location, sortBy).then( businesses => {
+    this.setState({businesses: businesses})
+   });
   }
 
   render(){
     return (
       <div className="App">
-        <h1>ravenous</h1>
+        <h1>Ravenous</h1>
         <SearchBar searchYelp={this.searchYelp} />
-        <BusinessList businesses={businesses} />
+        <BusinessList businesses={this.state.businesses} />
       </div>
     );
   }
